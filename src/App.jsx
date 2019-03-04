3bfb2ca47f5b534d2acdec7e2be7e58061b0f85e
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-04 11:27:28 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,28 @@ import { Provider } from 'react-redux';
 import Container from './components/Container';
 
 // 1- function that returns state
-function combinedState() {
-  return {
-    mentalHealthGaga: 100,
-    physicalHealthGaga: 100,
-  };
+const combinedState = combineReducers({ mentalHealth, physicalHealth });
+
+function mentalHealth(state = 100, action) {
+  switch (action.type) {
+    case 'WATCH_THE_NEWS':
+      return state - 20;
+    case 'WIN_THE_LOTTERY':
+      return state + 30;
+    default:
+      return state;
+  }
+}
+
+function physicalHealth(state = 100, action) {
+  switch (action.type) {
+    case 'SLIP_ON_BANANA_PEEL':
+      return state - 20;
+    case 'WIN_THE_LOTTERY':
+      return state + 10;
+    default:
+      return state;
+  }
 }
 
 // 2- let's actually make that state object
