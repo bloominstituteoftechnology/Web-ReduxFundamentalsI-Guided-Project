9f1bf0c6b6866d607e83c19256260b382210c286
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-04 10:57:08 +0100

    wip

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -4,28 +4,34 @@ import React from 'react';
 
 
 export default class Person extends React.Component {
-  state = {
-    mentalHealth: 100,
-    physicalHealth: 100,
-  }
+  // state = {
+  //   mentalHealth: 100,
+  //   physicalHealth: 100,
+  // }
 
   slipOnBananaPeel = () => {
-    this.setState(st => ({
-      physicalHealth: st.physicalHealth - 20,
-    }));
+    this.props.dispatch({ type: 'SLIP_ON_BANANA_PEEL' });
+
+    // this.setState(st => ({
+    //   physicalHealth: st.physicalHealth - 20,
+    // }));
   }
 
   watchTheNews = () => {
-    this.setState(st => ({
-      mentalHealth: st.mentalHealth - 20,
-    }));
+    this.props.dispatch({ type: 'WATCH_THE_NEWS' });
+
+    // this.setState(st => ({
+    //   mentalHealth: st.mentalHealth - 20,
+    // }));
   }
 
   winTheLottery = () => {
-    this.setState(st => ({
-      mentalHealth: st.mentalHealth + 30,
-      physicalHealth: st.physicalHealth + 10,
-    }));
+    this.props.dispatch({ type: 'WIN_THE_LOTTERY' });
+
+    // this.setState(st => ({
+    //   mentalHealth: st.mentalHealth + 30,
+    //   physicalHealth: st.physicalHealth + 10,
+    // }));
   }
 
   render() {
@@ -33,7 +39,7 @@ export default class Person extends React.Component {
       <div>
         <div>physical health: {this.state.physicalHealth}</div>
         <div>mental health: {this.state.mentalHealth}</div>
-
+        {/* children would get slices of state, and methods to change state */}
         <button onClick={this.slipOnBananaPeel}>Slip on banana peel</button>
         <button onClick={this.watchTheNews}>Watch the news</button>
         <button onClick={this.winTheLottery}>Win the Lottery</button>
