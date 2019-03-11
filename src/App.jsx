b04f8cc5d2b85eb7159060239d56010d8d57c9bc
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-11 11:27:07 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore, combineReducers } from 'redux';
-// import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import Container from './components/Container';
 
 
-// const store = createStore(
-//   combinedState,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-// );
+const appState = combineReducers({ mentalHealth, physicalHealth });
+
+function mentalHealth(state = 101, action) {
+  return state;
+}
+
+function physicalHealth(state = 99, action) {
+  return state;
+}
+
+const store = createStore(
+  appState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
-  <Container />,
+  <Provider store={store}>
+    <Container />
+  </Provider>,
   document.querySelector('#target'),
 );
