1ba1b232220fa753e467e5eedd72dd883b7fc754
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-11 10:53:26 +0100

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Person from './Person';
 
 
-export default function Container() {
-  return (
-    <div className='container'>
-      <Person />
-    </div>
-  );
+export default class Container extends React.Component {
+  state = {
+    mentalHealth: 100,
+    physicalHealth: 100,
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Person
+          mentalHealth={this.state.mentalHealth}
+          physicalHealth={this.state.physicalHealth}
+        />
+      </div>
+    );
+  }
 }
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-
 
 export default class Person extends React.Component {
-  state = {
-    mentalHealth: 100,
-    physicalHealth: 100,
-  }
-
   render() {
     return (
       <div>
-        <div>physical health: {this.state.physicalHealth}</div>
-        <div>mental health: {this.state.mentalHealth}</div>
+        <div>physical health: {this.props.physicalHealth}</div>
+        <div>mental health: {this.props.mentalHealth}</div>
 
-        <button>Slip on banana peel</button>
-        <button>Watch the news</button>
-        <button>Win the Lottery</button>
+        <button onClick={this.props.slipOnBanana}>Slip on banana peel</button>
+        <button onClick={this.props.watchTheNews}>Watch the news</button>
+        <button onClick={this.props.winTheLottery}>Win the Lottery</button>
       </div>
     );
   }
