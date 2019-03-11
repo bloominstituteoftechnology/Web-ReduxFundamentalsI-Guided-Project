dabc1b79c0a001eff522306526f6b4e3a4062b09
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-11 11:37:49 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,24 @@ import Container from './components/Container';
 const appState = combineReducers({ mentalHealth, physicalHealth });
 
 function mentalHealth(state = 101, action) {
+  if (action.type === 'WATCH_NEWS') {
+    return state - 20;
+  }
+  if (action.type === 'WIN_LOTTERY') {
+    return state + 30;
+  }
   return state;
 }
 
 function physicalHealth(state = 99, action) {
-  return state;
+  switch (action.type) {
+    case 'SLIP_ON_BANANA':
+      return state - 20;
+    case 'WIN_LOTTERY':
+      return state + 10;
+    default:
+      return state;
+  }
 }
 
 const store = createStore(
