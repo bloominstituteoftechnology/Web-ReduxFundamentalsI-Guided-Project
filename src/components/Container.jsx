14fc722323c6753f6b161c14afebce3709a0c777
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-03-11 10:56:52 +0100

    methods

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -8,12 +8,27 @@ export default class Container extends React.Component {
     physicalHealth: 100,
   }
 
+  slipOnBanana = () => {
+    console.log('slip on banana');
+  }
+
+  watchTheNews = () => {
+    console.log('watching the news');
+  }
+
+  winTheLottery = () => {
+    console.log('winning the lottery');
+  }
+
   render() {
     return (
       <div className='container'>
         <Person
           mentalHealth={this.state.mentalHealth}
           physicalHealth={this.state.physicalHealth}
+          slipOnBanana={this.slipOnBanana}
+          watchTheNews={this.watchTheNews}
+          winTheLottery={this.winTheLottery}
         />
       </div>
     );
