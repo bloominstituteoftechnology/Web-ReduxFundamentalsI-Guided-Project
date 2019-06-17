73e20b70b6cb4b288287d321e6ed198aee9ccc02
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-17 11:45:45 +0200

    wip

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 
-// STEP 1: WE NEED TO DEFAULT EXPORT THE "CONNECTED" VERSION
+// DISPATCH is a prop "connected" components get
+// and allows to dispatch "actions" that might trigger a state change
 class Person extends React.Component {
+  slipOnBanana = () => {
+    this.props.dispatch({ type: 'SLIP_ON_BANANA' });
+  }
+
   render() {
     return (
       <div>
         <div>physical health: {this.props.physicalHealth}</div>
         <div>mental health: {this.props.mentalHealth}</div>
 
-        <button onClick={this.props.slipOnBanana}>Slip on banana peel</button>
+        <button onClick={this.slipOnBanana}>Slip on banana peel</button>
         <button>Watch the news</button>
         <button>Win the Lottery</button>
       </div>
@@ -18,6 +23,7 @@ class Person extends React.Component {
   }
 }
 
+// STEP 1: WE NEED TO DEFAULT EXPORT THE "CONNECTED" VERSION
 // when we invoke connect WE GET A HOC BACK
 export default connect(
   state => state, // first arg is something that filters out the slices we don't need FUNCTION
