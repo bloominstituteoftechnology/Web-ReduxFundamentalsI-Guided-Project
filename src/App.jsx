16773a1c3ac3d4139c4b5ffe20f4d295f15dd373
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-17 10:29:50 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
 import Container from './components/Container';
 
+// STEP 1 --> find out what the state of the app should look like
+// { mentalHealth, physicalHealth }
+
+// STEP 2 --> think of the actions that might happen to alter these slices
+// and save them as constants (these are STRINGS we'll be using often)
+const SLIP_ON_BANANA = 'SLIP_ON_BANANA';
+const WATCH_THE_NEWS = 'WATCH_THE_NEWS';
+const WIN_LOTTERY = 'WIN_LOTTERY';
+
 
 // const store = createStore(
 //   combinedState,
