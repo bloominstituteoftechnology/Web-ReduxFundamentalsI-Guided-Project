1d3572b42916ecec1e33462ff25844233ed6efe4
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-17 12:01:04 +0200

    wip

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,32 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { SLIP_ON_BANANA, WATCH_THE_NEWS, WIN_LOTTERY } from '../App';
+import { slipOnBanana, watchNews, winLottery } from '../App';
 // import { bindActionCreators } from 'redux';
 
 // DISPATCH is a prop "connected" components get
 // and allows to dispatch "actions" that might trigger a state change
 class Person extends React.Component {
-  slipOnBanana = () => {
-    this.props.dispatch({ type: SLIP_ON_BANANA });
-  }
-
-  watchNews = () => {
-    this.props.dispatch({ type: WATCH_THE_NEWS });
-  }
-
-  winLottery = () => {
-    this.props.dispatch({ type: WIN_LOTTERY });
-  }
-
   render() {
     return (
       <div>
         <div>physical health: {this.props.physicalHealth}</div>
         <div>mental health: {this.props.mentalHealth}</div>
 
-        <button onClick={this.slipOnBanana}>Slip on banana peel</button>
-        <button onClick={this.watchNews}>Watch the news</button>
-        <button onClick={this.winLottery}>Win the Lottery</button>
+        <button onClick={this.props.slipOnBanana}>Slip on banana peel</button>
+        <button onClick={this.props.watchNews}>Watch the news</button>
+        <button onClick={this.props.winLottery}>Win the Lottery</button>
       </div>
     );
   }
@@ -35,11 +23,13 @@ class Person extends React.Component {
 // STEP 7: WE NEED TO DEFAULT EXPORT THE "CONNECTED" VERSION
 // when we invoke connect WE GET A HOC BACK
 export default connect(
-  // map state to props
+  // map state to props (use state => state if too confusing)
   state => ({
     mentalHealth: state.mentalHealth,
     physicalHealth: state.physicalHealth,
   }),
+  // an object with the action functions imported from app
+  { slipOnBanana, watchNews, winLottery },
 )(Person);
 
 // STEP 8: LET'S CREATE REUSABLE FUNCTION FOR SLIPPING ON BANANA ETC
