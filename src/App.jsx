4f83b04c0021673e2f90abb75b57c2561439be42
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-17 10:35:50 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,21 @@ import Container from './components/Container';
 
 // STEP 2 --> think of the actions that might happen to alter these slices
 // and save them as constants (these are STRINGS we'll be using often)
-const SLIP_ON_BANANA = 'SLIP_ON_BANANA';
-const WATCH_THE_NEWS = 'WATCH_THE_NEWS';
-const WIN_LOTTERY = 'WIN_LOTTERY';
+export const SLIP_ON_BANANA = 'SLIP_ON_BANANA';
+export const WATCH_THE_NEWS = 'WATCH_THE_NEWS';
+export const WIN_LOTTERY = 'WIN_LOTTERY';
+// import { SLIP_ON_BANANA } from './actionTypes'; // from someplace else
+// import * as actionTypes from './actionTypes'; // from someplace else
 
+// STEP 3 --> create ONE function per SLICE of state (2)
+// explaining how each of those 3 action types affect the given slice
+function mentalHealthReducer() {
+  
+}
+
+function physicalHealthReducer() {
+
+}
 
 // const store = createStore(
 //   combinedState,
